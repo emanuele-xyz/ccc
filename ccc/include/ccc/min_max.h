45550532262e27143ff75601d61555317d3154f5
emanuele <me.emanuele.xyz@gmail.com>
2023-04-25 20:43:53 +0200

    implement better vector types and simple min max macros

---
+++ ccc/include/ccc/min_max.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define ccc_min(a, b) ((a) < (b) ? (a) : (b))
+#define ccc_max(a, b) ((a) > (b) ? (a) : (b))
--- ccc/include/ccc/types.h
+++ ccc/include/ccc/types.h
@@ -7,29 +7,15 @@ typedef short     i16;
 typedef int       i32;
 typedef long long i64;
 
-typedef union v2i8 
-{
-    struct { i8 x, y; };
-    i8 v[2];
-} v2i8;
+typedef i8  v2i8 [2];
+typedef i16 v2i16[2];
+typedef i32 v2i32[2];
+typedef i64 v2i64[2];
 
-typedef union v2i16 
-{
-    struct { i16 x, y; };
-    i16 v[2];
-} v2i16;
-
-typedef union v2i32 
-{
-    struct { i32 x, y; };
-    i32 v[2];
-} v2i32;
-
-typedef union v2i64 
-{
-    struct { i64 x, y; };
-    i64 v[2];
-} v2i64;
+typedef i8  v3i8 [3];
+typedef i16 v3i16[3];
+typedef i32 v3i32[3];
+typedef i64 v3i64[3];
 
 ccc_static_assert(sizeof(i8)  == 1);
 ccc_static_assert(sizeof(i16) == 2);
@@ -41,29 +27,15 @@ typedef unsigned short     u16;
 typedef unsigned int       u32;
 typedef unsigned long long u64;
 
-typedef union v2u8 
-{
-    struct { u8 x, y; };
-    u8 v[2];
-} v2u8;
+typedef u8  v2u8 [2];
+typedef u16 v2u16[2];
+typedef u32 v2u32[2];
+typedef u64 v2u64[2];
 
-typedef union v2u16 
-{
-    struct { u16 x, y; };
-    u16 v[2];
-} v2u16;
-
-typedef union v2u32 
-{
-    struct { u32 x, y; };
-    u32 v[2];
-} v2u32;
-
-typedef union v2u64 
-{
-    struct { u64 x, y; };
-    u64 v[2];
-} v2u64;
+typedef u8  v3u8 [3];
+typedef u16 v3u16[3];
+typedef u32 v3u32[3];
+typedef u64 v3u64[3];
 
 ccc_static_assert(sizeof(u8)  == 1);
 ccc_static_assert(sizeof(u16) == 2);
@@ -83,17 +55,26 @@ ccc_static_assert(sizeof(b64) == 8);
 typedef float  f32;
 typedef double f64;
 
-typedef union v2f32 
-{
-    struct { f32 x, y; };
-    f32 v[2];
-} v2f32;
+typedef f32 v2f32[2];
+typedef f64 v2f64[2];
 
-typedef union v2f64 
-{
-    struct { f64 x, y; };
-    f64 v[2];
-} v2f64;
+typedef f32 v3f32[3];
+typedef f64 v3f64[3];
 
 ccc_static_assert(sizeof(f32) == 4);
 ccc_static_assert(sizeof(f64) == 8);
+
+enum
+{
+	V_X = 0,
+	V_Y = 1,
+	V_Z = 2,
+
+	V_R = 0,
+	V_G = 1,
+	V_B = 2,
+
+	V_W = 0,
+	V_H = 1,
+	V_D = 2,
+};
