886463bfa53202971df2f4e0f3623ce22c39fb2e
emanuele <me.emanuele.xyz@gmail.com>
2023-05-15 20:51:01 +0200

    fix fqueue bug: allocate enough memory for both header and data segment

--- ccc/include/ccc/fqueue.h
+++ ccc/include/ccc/fqueue.h
@@ -13,6 +13,110 @@
 	u64 length:    queue's length
 */
 
+/*
+	Suppose we have the following fqueue
+		elem_size: 8
+		capacity:  4
+		start:     0
+		length:    3
+
+		+---+---+---+---+
+		| a | b | c | . |
+		+---+---+---+---+
+
+	After enqueuing element d we have that
+		elem_size: 8
+		capacity:  4
+		start:     0
+		length:    4 <-- update
+
+		+---+---+---+---+
+		| a | b | c | d |
+		+---+---+---+---+
+
+	Suppose we have the following fqueue
+		elem_size: 8
+		capacity:  4
+		start:     1
+		length:    3
+
+		+---+---+---+---+
+		| . | a | b | c |
+		+---+---+---+---+
+
+	After enqueuing element d we have that
+		elem_size: 8
+		capacity:  4
+		start:     1
+		length:    4 <-- update
+
+		+---+---+---+---+
+		| d | a | b | c |
+		+---+---+---+---+
+*/
+
+/*
+	Suppose we have the following fqueue
+		elem_size: 8
+		capacity:  4
+		start:     0
+		length:    3
+
+		+---+---+---+---+
+		| a | b | c | . |
+		+---+---+---+---+
+
+	After dequeuing (element a) we have that
+		elem_size: 8
+		capacity:  4
+		start:     1 <-- update
+		length:    2 <-- update
+
+		+---+---+---+---+
+		| . | b | c | . |
+		+---+---+---+---+
+
+	Suppose we have the following fqueue
+		elem_size: 8
+		capacity:  4
+		start:     1
+		length:    3
+
+		+---+---+---+---+
+		| . | a | b | c |
+		+---+---+---+---+
+
+	After dequeuing (element a) we have that
+		elem_size: 8
+		capacity:  4
+		start:     2 <-- update
+		length:    2 <-- update
+
+		+---+---+---+---+
+		| . | . | b | c |
+		+---+---+---+---+
+
+	Suppose we have the following fqueue
+		elem_size: 8
+		capacity:  4
+		start:     3
+		length:    2
+
+		+---+---+---+---+
+		| b | . | . | a |
+		+---+---+---+---+
+
+	After dequeuing (element a) we have that
+		elem_size: 8
+		capacity:  4
+		start:     0 <-- update
+		length:    1 <-- update
+
+		+---+---+---+---+
+		| b | . | . | . |
+		+---+---+---+---+
+*/
+
 typedef struct ccc_fqueue
 {
 	u64 elem_size;
@@ -28,4 +132,5 @@ typedef struct ccc_fqueue
 
 ccc_err ccc_fqueue_init(u64 elem_size, u64 capacity, void** out);
 void ccc_fqueue_fini(void* q);
-
+ccc_err ccc_fqueue_enqueue(void* q, void* v);
+void ccc_fqueue_dequeue(void* q, void* v);
--- ccc/src/fqueue.c
+++ ccc/src/fqueue.c
@@ -1,24 +1,13 @@
 #include <ccc/fqueue.h>
 #include <ccc/alloc.h>
 
-/*
-	TODO: move in .h file 
- 
-	fixed size queue
-
-	u64 elem_size: element size
-	u64 capacity:  maximum number of elements
-	u64 start:     queue's starting index
-	u64 length:    queue's length
-*/
-
 ccc_err ccc_fqueue_init(u64 elem_size, u64 capacity, void** out)
 {
 	ccc_err err = CCC_OK;
 	b8 res = CCC_T;
 
 	ccc_fqueue* tmp = 0;
-	err = ccc_alloc(elem_size * capacity, &tmp);
+	err = ccc_alloc(sizeof(ccc_fqueue) + elem_size * capacity, &tmp);
 	res = res && ccc_ok(err);
 	if (res)
 	{
@@ -37,50 +26,6 @@ void ccc_fqueue_fini(void* q)
 	ccc_free(((ccc_fqueue*)(q)-1));
 }
 
-/*
-	TODO: move in .h file
-
-	Suppose we have the following fqueue
-		elem_size: 8
-		capacity:  4
-		start:     0
-		length:    3
-
-		+---+---+---+---+
-		| a | b | c | . |
-		+---+---+---+---+
-
-	After enqueuing element d we have that
-		elem_size: 8
-		capacity:  4
-		start:     0
-		length:    4 <-- update
-
-		+---+---+---+---+
-		| a | b | c | d |
-		+---+---+---+---+
-
-	Suppose we have the following fqueue
-		elem_size: 8
-		capacity:  4
-		start:     1
-		length:    3
-
-		+---+---+---+---+
-		| . | a | b | c |
-		+---+---+---+---+
-
-	After enqueuing element d we have that
-		elem_size: 8
-		capacity:  4
-		start:     1
-		length:    4 <-- update
-
-		+---+---+---+---+
-		| d | a | b | c |
-		+---+---+---+---+
-*/
-
 ccc_err ccc_fqueue_enqueue(void* q, void* v)
 {
 	ccc_err err = CCC_OK;
@@ -97,77 +42,13 @@ ccc_err ccc_fqueue_enqueue(void* q, void* v)
 	{
 		for (u64 i = 0; i < elem_size; i++)
 		{
-			((char*)(q))[((start + length) % capacity) * elem_size + i] = ((char*)(v))[i];
+			((char*)q)[((start + length) % capacity) * elem_size + i] = ((char*)(v))[i];
 		}
 		ccc_fqueue_length(q)++;
 	}
 
 	return err;
 }
-
-/*
-	TODO: move in .h file
- 
-	Suppose we have the following fqueue
-		elem_size: 8
-		capacity:  4
-		start:     0
-		length:    3
-
-		+---+---+---+---+
-		| a | b | c | . |
-		+---+---+---+---+
-
-	After dequeuing (element a) we have that
-		elem_size: 8
-		capacity:  4
-		start:     1 <-- update
-		length:    2 <-- update
-
-		+---+---+---+---+
-		| . | b | c | . |
-		+---+---+---+---+
-
-	Suppose we have the following fqueue
-		elem_size: 8
-		capacity:  4
-		start:     1
-		length:    3
-
-		+---+---+---+---+
-		| . | a | b | c |
-		+---+---+---+---+
-
-	After dequeuing (element a) we have that
-		elem_size: 8
-		capacity:  4
-		start:     2 <-- update
-		length:    2 <-- update
-
-		+---+---+---+---+
-		| . | . | b | c |
-		+---+---+---+---+
-
-	Suppose we have the following fqueue
-		elem_size: 8
-		capacity:  4
-		start:     3
-		length:    2
-
-		+---+---+---+---+
-		| b | . | . | a |
-		+---+---+---+---+
-
-	After dequeuing (element a) we have that
-		elem_size: 8
-		capacity:  4
-		start:     0 <-- update
-		length:    1 <-- update
-
-		+---+---+---+---+
-		| b | . | . | . |
-		+---+---+---+---+
-*/
 
 void ccc_fqueue_dequeue(void* q, void* v)
 {
