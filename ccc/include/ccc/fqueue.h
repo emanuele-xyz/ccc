0fbc5f4b6b3415a1d520305be5a87286bd34631a
emanuele <me.emanuele.xyz@gmail.com>
2023-05-28 18:05:20 +0200

    improve fqueue documentation

--- ccc/include/ccc/fqueue.h
+++ ccc/include/ccc/fqueue.h
@@ -4,6 +4,26 @@
 #include <ccc/errors.h>
 
 /*
+	u64 elem_size: size of element in bytes
+	u64 capacity:  maximum number of elements
+	u64 start:     index to the first element in the queue
+	u64 length:    length
+*/
+
+typedef struct ccc_fqueue
+{
+	u64 elem_size;
+	u64 capacity;
+	u64 start;
+	u64 length;
+} ccc_fqueue;
+
+#define ccc_fqueue_elem_size(q) (((ccc_fqueue*)(q) - 1)->elem_size)
+#define ccc_fqueue_capacity(q)  (((ccc_fqueue*)(q) - 1)->capacity)
+#define ccc_fqueue_length(q)    (((ccc_fqueue*)(q) - 1)->length)
+#define ccc_fqueue_start(q)     (((ccc_fqueue*)(q) - 1)->start)
+
+/*
 	Suppose we have the following fqueue
 		elem_size: 8
 		capacity:  4
@@ -106,19 +126,6 @@
 		| b | . | . | . |
 		+---+---+---+---+
 */
-
-typedef struct ccc_fqueue
-{
-	u64 elem_size;
-	u64 capacity;
-	u64 start;
-	u64 length;
-} ccc_fqueue;
-
-#define ccc_fqueue_elem_size(q) (((ccc_fqueue*)(q) - 1)->elem_size)
-#define ccc_fqueue_capacity(q)  (((ccc_fqueue*)(q) - 1)->capacity)
-#define ccc_fqueue_length(q)    (((ccc_fqueue*)(q) - 1)->length)
-#define ccc_fqueue_start(q)     (((ccc_fqueue*)(q) - 1)->start)
 
 ccc_err ccc_fqueue_init(u64 elem_size, u64 capacity, void** out);
 void ccc_fqueue_fini(void* q);
